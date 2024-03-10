9d9a6c9c878f26b202a7410d7ff92844bffa9bcd
Soumil Biswas <soumil.biswas19@gmail.com>
2024-03-10 04:03:56 -0700

    Implemented code to generate NxN grid

--- javascript.js
+++ javascript.js
@@ -1,7 +1,44 @@
-const graphSize = prompt("Enter graph size N to make a NxN grid. N must be less than 100.");
-if (graphSize > 100){
-    alert("Grpah size too big! N must be less than 100.");
+function createBox(){
+    const box = document.createElement('div');
+    box.setAttribute('class', 'box');
+    
+    return box;
 }
-else{
-    createGraph(graphSize);
+
+function createRow(n){
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    for(let i=0; i<n; i++){
+        const box = createBox();
+        row.appendChild(box);
+    }
+    return row;
 }
+
+function createGraph(n){
+    const body = document.querySelector('body');
+    const sketchpad = document.createElement('div');
+    sketchpad.setAttribute('class', 'sketchpad');
+    
+    for(let i=0; i<n; i++){
+        const row = createRow(n);
+        sketchpad.appendChild(row);
+    }
+
+    body.appendChild(sketchpad);
+}
+
+//Initial Prompt
+function init{
+    const graphSize = prompt("Enter graph size N to make a NxN grid. N must be less than 100.");
+    if (graphSize > 100){
+        alert("Grpah size too big! N must be less than 100.");
+        init();
+    }
+    else{
+        createGraph(graphSize);
+    }
+}
+
+// Driver Code
+init();
